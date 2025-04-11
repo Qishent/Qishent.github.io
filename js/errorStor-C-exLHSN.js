c8e2224cd8afa18f6a80bdfbe0228e07f8a52397
Qishent <3283089140@qq.com>
2025-04-11 10:36:11 +0800

    第三次更新

---
+++ js/errorStor-C-exLHSN.js
@@ -0,0 +1,1 @@
+import{a1 as s}from"./index-BAA_AMh7.js";const o=s("errorStore",{state:()=>({statusCode:null,errorMessage:null}),actions:{setError(r,e){this.statusCode=r,this.errorMessage=e}}});export{o as u};
--- js/errorStor-CelPOucA.js
+++
@@ -1,1 +0,0 @@
-import{a1 as s}from"./index-BWPHe3CG.js";const o=s("errorStore",{state:()=>({statusCode:null,errorMessage:null}),actions:{setError(r,e){this.statusCode=r,this.errorMessage=e}}});export{o as u};
